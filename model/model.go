75d05a203f2aac0a27d498083dd286ff1f071825
Mohan Kumar A <mohankumarnimo2@gmail.com>
2024-02-09 13:49:22 +0530

    Update model.go

--- model/model.go
+++ model/model.go
@@ -16,6 +16,18 @@ type Input struct {
 	ATRK2  string `json:"atrk2"`
 	ATRV2  string `json:"atrv2"`
 	ATRT2  string `json:"atrt2"`
+	ATRK3  string `json:"atrk3"`
+	ATRV3  string `json:"atrv3"`
+	ATRT3  string `json:"atrt3"`
+	ATRK4  string `json:"atrk4"`
+	ATRV4  string `json:"atrv4"`
+	ATRT4  string `json:"atrt4"`
+	ATRK5  string `json:"atrk5"`
+	ATRV5  string `json:"atrv5"`
+	ATRT5  string `json:"atrt5"`
+	ATRK6  string `json:"atrk6"`
+	ATRV6  string `json:"atrv6"`
+	ATRT6  string `json:"atrt6"`
 	UATRK1 string `json:"uatrk1"`
 	UATRV1 string `json:"uatrv1"`
 	UATRT1 string `json:"uatrt1"`
@@ -25,6 +37,15 @@ type Input struct {
 	UATRK3 string `json:"uatrk3"`
 	UATRV3 string `json:"uatrv3"`
 	UATRT3 string `json:"uatrt3"`
+	UATRK4 string `json:"uatrk4"`
+	UATRV4 string `json:"uatrv4"`
+	UATRT4 string `json:"uatrt4"`
+	UATRK5 string `json:"uatrk5"`
+	UATRV5 string `json:"uatrv5"`
+	UATRT5 string `json:"uatrt5"`
+	UATRK6 string `json:"uatrk6"`
+	UATRV6 string `json:"uatrv6"`
+	UATRT6 string `json:"uatrt6"`
 }
 
 type Output struct {
